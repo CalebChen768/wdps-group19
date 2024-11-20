db32570d45ef6f70463cdf0e237061e668b62110
CalebChen768 <72332844+CalebChen768@users.noreply.github.com>
2024-11-20 17:19:40 +0800

    add blank eneity linking class

---
+++ group-19/entity_linking.py
@@ -0,0 +1,16 @@
+
+# Entity Linking class
+class EL:
+    def __init__(self):
+        pass
+
+    def generate_candidates(self, entities):
+        pass
+
+    def rank_candidates(self, candidates):
+        pass
+
+    # .....
+
+if __name__=="__main__":
+    pass
